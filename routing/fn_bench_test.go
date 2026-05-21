6cc746ee2b68d01f8822f75396ee71749e42d247
Đăng Được <duoc200894@gmail.com>
2026-05-21 22:16:16 +0700

    perf(routing): pre-compile regexes and fix Group slice aliasing

---
+++ routing/fn_bench_test.go
@@ -0,0 +1,38 @@
+package routing
+
+import (
+	"net/http"
+	"testing"
+)
+
+func BenchmarkPatternToMethodRouteVersion(b *testing.B) {
+	pattern := MethodRouteVersionToPattern(http.MethodGet, "/users/{userId}/friends/{friendId}", "v2")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		PatternToMethodRouteVersion(pattern)
+	}
+}
+
+func BenchmarkParseToParamKey(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ParseToParamKey("/users/$/friends/$/schools/$/subjects/$/")
+	}
+}
+
+func BenchmarkMatchWildcard(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		matchWildcard("index.html", "in*.html")
+	}
+}
+
+func BenchmarkToEndpoint(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ToEndpoint("//users//profile//settings//")
+	}
+}
+
+func BenchmarkMethodRouteVersionToPattern(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		MethodRouteVersionToPattern(http.MethodGet, "/users/{userId}/all", "v2")
+	}
+}
