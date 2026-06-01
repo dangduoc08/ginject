8276f38de6b9ee909232154ec15ba88022939673
Đăng Được <duoc200894@gmail.com>
2026-06-02 02:07:41 +0700

    refactor(middlewares): split each middleware into its own subpackage

---
+++ middlewares/requestlogger/requestlogger.go
@@ -0,0 +1,43 @@
+package requestlogger
+
+import (
+	"time"
+
+	"github.com/dangduoc08/ginject/broker"
+	"github.com/dangduoc08/ginject/common"
+	"github.com/dangduoc08/ginject/ctx"
+)
+
+type RequestLogger struct {
+	common.Logger
+}
+
+func (instance RequestLogger) Use(c *ctx.Context, next ctx.Next) {
+	_, _ = c.Broker.Subscribe(ctx.REQUEST_FINISHED, func(m *broker.Message) {
+		newC := m.Payload.(*ctx.Context)
+		requestType := newC.GetType()
+		responseTime := time.Since(newC.Timestamp)
+
+		switch requestType {
+		case ctx.HTTPType:
+			instance.Info(
+				newC.URL.String(),
+				"Method", newC.Method,
+				"Status", newC.Code,
+				"Time", responseTime.String(),
+				"Protocol", newC.Proto,
+				"User-Agent", newC.UserAgent(),
+				ctx.REQUEST_ID, newC.GetID(),
+			)
+		case ctx.WSType:
+			instance.Info(
+				newC.WS.Message.Event,
+				"Time", responseTime.String(),
+				"Subprotocol", newC.WS.GetSubprotocol(),
+				"User-Agent", newC.UserAgent(),
+			)
+		}
+	})
+
+	next()
+}
