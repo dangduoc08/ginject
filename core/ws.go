e08f1cc4c1dbdb7573253bbb212d0fae544274eb
Đăng Được <duoc200894@gmail.com>
2026-06-01 13:20:59 +0700

    fix(core): eliminate defer accumulation and stale c.Next in ws message loop

--- core/ws.go
+++ core/ws.go
@@ -41,12 +41,9 @@ func newWS() *WS {
 func (ws *WS) handleRequest(wsConn *websocket.Conn, c *ctx.Context) {
 	wsInstance := ctx.NewWS(wsConn)
 	c.WS = wsInstance
-	isNext := true
-	c.Next = func() {
-		isNext = true
-	}
 	wsid := wsInstance.GetConnID()
 	wsSubscribedEvents := wsInstance.GetSubscribedEvents()
+	subprotocol := wsInstance.GetSubprotocol()
 
 	defer func() {
 		for _, subscribedEventName := range wsSubscribedEvents {
@@ -66,18 +63,11 @@ func (ws *WS) handleRequest(wsConn *websocket.Conn, c *ctx.Context) {
 	}
 
 	for {
-
-		// listen on comming messages
 		var message []byte
 		err := websocket.Message.Receive(wsConn, &message)
-
-		// reset timestamp
-		// based on time when receive message
 		c.Timestamp = time.Now()
 
 		if err != nil {
-
-			// client close connection
 			if err == io.EOF {
 				break
 			}
@@ -86,129 +76,113 @@ func (ws *WS) handleRequest(wsConn *websocket.Conn, c *ctx.Context) {
 		}
 
 		var wsMsg ctx.WSMessage
-		err = json.Unmarshal(message, &wsMsg)
-		if err != nil {
+		if err = json.Unmarshal(message, &wsMsg); err != nil {
 			ws.invokeGlobalMiddlewares(c, exception.UnsupportedMediaTypeException(err.Error()))
 			continue
 		}
 
-		// event was registered by controller
-		var publishEventName string
-		defer func() {
-			if rec := recover(); rec != nil {
+		isNext := true
+		c.Next = func() { isNext = true }
 
-				// Pipe errors run first
-				// then exception filter
-				if errorAggregationOperators, ok := c.Context().Value(WithValueKey(aggregation.ERROR_AGGREGATION_CTX_VALUE_KEY)).([]aggregation.AggregationOperator); ok {
-					totalErrorAggregations := len(errorAggregationOperators)
-
-					// Handle case if pipe error panic
-					defer func() {
-						if rec := recover(); rec != nil {
-							c.Event.Emit(publishEventName, c, rec, 0)
-						}
-					}()
-
-					for i := totalErrorAggregations - 1; i >= 0; i-- {
-						aggregation := errorAggregationOperators[i]
-						rec = aggregation(c, rec)
-					}
-				}
-
-				// Execute exception filters if any
-				// normally this one always ok
-				// since we always set global exception filter as default
-				if _, ok := ws.catchFnsMap[publishEventName]; ok && rec != nil {
-
-					// 3rd param is index of catch function
-					c.Event.Emit(publishEventName, c, rec, 0)
-				}
-
-				// reset ErrorAggregationOperators
-				// to prevent duplicate error aggregation
-				// due to error will be added
-				// whenever interceptor triggered
-				// but WS 1 connection use 1 ctx
-				newCtx := context.WithValue(c.Context(), WithValueKey(aggregation.ERROR_AGGREGATION_CTX_VALUE_KEY), nil)
-				c.Request = c.WithContext(newCtx)
-
-				// clean all events before recursion
-				// prevent emit duplicate event
-				for _, eventName := range wsSubscribedEvents {
-					ws.removeWSEvent(eventName, wsid, c)
-				}
-
-				// recursion to keep connection alive
-				ws.handleRequest(wsConn, c)
-			}
-		}()
-
-		c.WS.Message = wsMsg
-		publishEventName = common.ToWSEventName(wsInstance.GetSubprotocol(), wsMsg.Event)
-
-		if handlers, isMatched := ws.eventMap[publishEventName]; isMatched {
-			for index, handler := range handlers {
-				if isNext {
-					isNext = false
-					handler(c)
-
-					// when ran through all middlewares
-					// then invoke mainhandler
-					if index == len(handlers)-1 && isNext {
-						injectableHandler := ws.mainHandlerMap[publishEventName]
-
-						// data return from main handler
-						data := ws.invokeHandler(injectableHandler, c)
-						if len(data) == 1 {
-							data = append(data, reflect.ValueOf("*"))
-							data[1], data[0] = data[0], data[1]
-						}
-						configPublishedEventName := data[0].String()
-
-						if aggregations, ok := c.Context().Value(WithValueKey(publishEventName)).([]*aggregation.Aggregation); ok {
-							var aggregatedData any
-							isMainHandlerCalled := true
-
-							totalAggregations := len(aggregations)
-
-							for i := totalAggregations - 1; i >= 0; i-- {
-								aggregation := aggregations[i]
-
-								if aggregation.IsMainHandlerCalled {
-
-									// set data from main handler into
-									// first interceptor
-									if i == totalAggregations-1 && len(data) > 1 {
-										aggregatedData = data[1].Interface()
-									}
-
-									aggregation.SetMainData(aggregatedData)
-									aggregatedData = aggregation.Aggregate(c)
-								} else {
-									isMainHandlerCalled = false
-									wsMsg := toWSMessage(reflect.ValueOf(aggregation.InterceptorData))
-									ws.publishWSEvent(configPublishedEventName, wsMsg, c)
-									break
-								}
-							}
-
-							if isMainHandlerCalled {
-								wsMsg := toWSMessage(reflect.ValueOf(aggregatedData))
-								ws.publishWSEvent(configPublishedEventName, wsMsg, c)
-							}
-						} else {
-							if len(data) > 1 {
-								wsMsg := toWSMessage(data[1])
-								ws.publishWSEvent(configPublishedEventName, wsMsg, c)
-							}
-						}
-					}
-				}
-			}
-		} else {
-			ws.invokeGlobalMiddlewares(c, exception.NotFoundException("Cannot emit "+wsMsg.Event+" event"))
+		if ws.dispatchMessage(c, wsConn, wsMsg, subprotocol, &isNext, wsid, wsSubscribedEvents) {
+			return
 		}
 	}
+}
+
+func (ws *WS) dispatchMessage(c *ctx.Context, wsConn *websocket.Conn, wsMsg ctx.WSMessage, subprotocol string, isNext *bool, wsid string, wsSubscribedEvents []string) (recurse bool) {
+	publishEventName := common.ToWSEventName(subprotocol, wsMsg.Event)
+
+	defer func() {
+		if rec := recover(); rec != nil {
+			if errorAggregationOperators, ok := c.Context().Value(WithValueKey(aggregation.ERROR_AGGREGATION_CTX_VALUE_KEY)).([]aggregation.AggregationOperator); ok {
+				totalErrorAggregations := len(errorAggregationOperators)
+
+				defer func() {
+					if rec := recover(); rec != nil {
+						c.Event.Emit(publishEventName, c, rec, 0)
+					}
+				}()
+
+				for i := totalErrorAggregations - 1; i >= 0; i-- {
+					op := errorAggregationOperators[i]
+					rec = op(c, rec)
+				}
+			}
+
+			if _, ok := ws.catchFnsMap[publishEventName]; ok && rec != nil {
+				c.Event.Emit(publishEventName, c, rec, 0)
+			}
+
+			newCtx := context.WithValue(c.Context(), WithValueKey(aggregation.ERROR_AGGREGATION_CTX_VALUE_KEY), nil)
+			c.Request = c.WithContext(newCtx)
+
+			for _, eventName := range wsSubscribedEvents {
+				ws.removeWSEvent(eventName, wsid, c)
+			}
+
+			ws.handleRequest(wsConn, c)
+			recurse = true
+		}
+	}()
+
+	c.WS.Message = wsMsg
+
+	if handlers, isMatched := ws.eventMap[publishEventName]; isMatched {
+		for index, handler := range handlers {
+			if *isNext {
+				*isNext = false
+				handler(c)
+
+				if index == len(handlers)-1 && *isNext {
+					injectableHandler := ws.mainHandlerMap[publishEventName]
+
+					data := ws.invokeHandler(injectableHandler, c)
+					if len(data) == 1 {
+						data = append(data, reflect.ValueOf("*"))
+						data[1], data[0] = data[0], data[1]
+					}
+					configPublishedEventName := data[0].String()
+
+					if aggregations, ok := c.Context().Value(WithValueKey(publishEventName)).([]*aggregation.Aggregation); ok {
+						var aggregatedData any
+						isMainHandlerCalled := true
+						totalAggregations := len(aggregations)
+
+						for i := totalAggregations - 1; i >= 0; i-- {
+							agg := aggregations[i]
+
+							if agg.IsMainHandlerCalled {
+								if i == totalAggregations-1 && len(data) > 1 {
+									aggregatedData = data[1].Interface()
+								}
+								agg.SetMainData(aggregatedData)
+								aggregatedData = agg.Aggregate(c)
+							} else {
+								isMainHandlerCalled = false
+								wsMsg2 := toWSMessage(reflect.ValueOf(agg.InterceptorData))
+								ws.publishWSEvent(configPublishedEventName, wsMsg2, c)
+								break
+							}
+						}
+
+						if isMainHandlerCalled {
+							wsMsg2 := toWSMessage(reflect.ValueOf(aggregatedData))
+							ws.publishWSEvent(configPublishedEventName, wsMsg2, c)
+						}
+					} else {
+						if len(data) > 1 {
+							wsMsg2 := toWSMessage(data[1])
+							ws.publishWSEvent(configPublishedEventName, wsMsg2, c)
+						}
+					}
+				}
+			}
+		}
+	} else {
+		ws.invokeGlobalMiddlewares(c, exception.NotFoundException("Cannot emit "+wsMsg.Event+" event"))
+	}
+	return false
 }
 
 func (ws *WS) addWSEvent(subscribedEventName, wsid string, c *ctx.Context, cb func(args ...any)) {
@@ -264,30 +238,3 @@ func (ws *WS) invokeGlobalMiddlewares(c *ctx.Context, exception exception.Except
 		})
 	}
 }
-
-// package main
-
-// import (
-// 	"net/http"
-
-// 	"golang.org/x/net/websocket"
-// )
-
-// func wsHandler(ws *websocket.Conn) {
-// 	defer ws.Close()
-// }
-
-// func main() {
-// 	s := websocket.Server{
-// 		Handler: websocket.Handler(wsHandler),
-
-// 		Handshake: func(cfg *websocket.Config, req *http.Request) error {
-// 			// Cho phép mọi Origin
-// 			return nil
-// 		},
-// 	}
-
-// 	http.Handle("/ws", s)
-
-// 	http.ListenAndServe(":4000", nil)
-// }
