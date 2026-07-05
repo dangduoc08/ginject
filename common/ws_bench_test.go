80bfca19e9687b26fb19f3761588e8b966dbf0ed
Đăng Được <duoc200894@gmail.com>
2026-07-05 20:53:19 +0700

    refactor(common): rename WS operation prefix from ON to SUBSCRIBE

--- common/ws_bench_test.go
+++ common/ws_bench_test.go
@@ -4,10 +4,10 @@ import "testing"
 
 func BenchmarkAddHandlerToEventMap(b *testing.B) {
 	fns := []string{
-		"ON_chat_message", "ON_chat_status", "ON_chat_ANY",
-		"ON_room_join", "ON_room_leave", "ON_room_ALL",
-		"ON_user_typing", "ON_user_presence", "ON_notification",
-		"ON_ALL",
+		"SUBSCRIBE_chat_message", "SUBSCRIBE_chat_status", "SUBSCRIBE_chat_ANY",
+		"SUBSCRIBE_room_join", "SUBSCRIBE_room_leave", "SUBSCRIBE_room_ALL",
+		"SUBSCRIBE_user_typing", "SUBSCRIBE_user_presence", "SUBSCRIBE_notification",
+		"SUBSCRIBE_ALL",
 	}
 	b.ResetTimer()
 	for range b.N {
@@ -23,7 +23,7 @@ func BenchmarkAddHandlerToEventMap(b *testing.B) {
 
 func BenchmarkWSFnToSubject(b *testing.B) {
 	fns := []string{
-		"ON_chat_message", "ON_chat_ANY", "ON_chat_ALL", "ON_ALL",
+		"SUBSCRIBE_chat_message", "SUBSCRIBE_chat_ANY", "SUBSCRIBE_chat_ALL", "SUBSCRIBE_ALL",
 	}
 	b.ResetTimer()
 	for range b.N {
