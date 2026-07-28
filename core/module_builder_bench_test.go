e28d940ba226e78a954f48f384392778db4441ba
Đăng Được <duoc200894@gmail.com>
2026-07-29 02:25:51 +0700

    test(core): add benchmarks for module bootstrap hot paths

--- core/module_builder_bench_test.go
+++ core/module_builder_bench_test.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"os"
+	"strconv"
 	"testing"
 )
 
@@ -22,5 +24,30 @@ func BenchmarkGetModuleType(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		builder.getModuleType()
+	}
+}
+
+func BenchmarkParseModuleFile_DistinctFiles(b *testing.B) {
+	const n = 500
+	dir := b.TempDir()
+	paths := make([]string, n)
+	for i := range paths {
+		path := dir + "/f" + strconv.Itoa(i) + ".go"
+		src := "package pkg" + strconv.Itoa(i) + "\n\nvar Module = func() *int { return nil }\n"
+		if err := os.WriteFile(path, []byte(src), 0o600); err != nil {
+			b.Fatal(err)
+		}
+		paths[i] = path
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		path := paths[i%n]
+
+		b.StopTimer()
+		moduleNameFileCache.Delete(path)
+		b.StartTimer()
+
+		parseModuleFile(path)
 	}
 }
