9e5b486232958c349742c7aea182c42bf2c17457
Đăng Được <duoc200894@gmail.com>
2026-08-04 01:12:25 +0700

    refactor(aggregation): remove dead code and complete Filter operator implementation

--- aggregation/aggregation_bench_test.go
+++ aggregation/aggregation_bench_test.go
@@ -5,6 +5,7 @@ import (
 )
 
 var benchNoop = func(data any) any { return data }
+var benchPredicate = func(data any) bool { return true }
 
 func BenchmarkNewAggregation(b *testing.B) {
 	for i := 0; i < b.N; i++ {
@@ -42,24 +43,6 @@ func BenchmarkAggregate_AllOperators(b *testing.B) {
 	}
 }
 
-func BenchmarkGetAggregationOperators_Hit(b *testing.B) {
-	a := NewAggregation()
-	a.Transform(benchNoop)
-	a.Tap(benchNoop)
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = a.GetAggregationOperators(OperatorTap)
-	}
-}
-
-func BenchmarkGetAggregationOperators_Miss(b *testing.B) {
-	a := NewAggregation()
-	a.Transform(benchNoop)
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = a.GetAggregationOperators(OperatorTap)
-	}
-}
 
 func BenchmarkSetOperators_Two(b *testing.B) {
 	b.ResetTimer()
@@ -67,5 +50,47 @@ func BenchmarkSetOperators_Two(b *testing.B) {
 		a := NewAggregation()
 		a.Transform(benchNoop)
 		a.Tap(benchNoop)
+	}
+}
+
+func BenchmarkAggregate_Filter(b *testing.B) {
+	a := NewAggregation()
+	a.SetMainData(10)
+	a.Filter(benchPredicate)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		a.Aggregate()
+	}
+}
+
+func BenchmarkAggregate_Filter_False(b *testing.B) {
+	a := NewAggregation()
+	a.SetMainData(10)
+	a.Filter(func(data any) bool { return false })
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		a.Aggregate()
+	}
+}
+
+func BenchmarkAggregate_MultipleFilters(b *testing.B) {
+	a := NewAggregation()
+	a.SetMainData(30)
+	a.Filter(benchPredicate)
+	a.Filter(benchPredicate)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		a.Aggregate()
+	}
+}
+
+func BenchmarkAggregate_Filter_And_Transform(b *testing.B) {
+	a := NewAggregation()
+	a.SetMainData("data")
+	a.Filter(benchPredicate)
+	a.Transform(benchNoop)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		a.Aggregate()
 	}
 }
